567b064848b341771f782e50dc641fc1a0d6b3cf
Luis Arturo Mendoza <arturo10.mr@gmail.com>
2017-10-05 19:33:05 -0500

    General Swap function

---
+++ swap/swap.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "swap.h"
+
+void swap(void *a, void *b, int size){
+  char *buffer;
+  buffer = (char *)malloc(sizeof(char)*size);
+  memcpy(buffer,a,size);
+  memcpy(a,b,size);
+  memcpy(b,buffer,size);
+  free(buffer);
+}
