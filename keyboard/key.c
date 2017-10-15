f381cf47b478889a706fa3fbeaccec73a89548a6
Luis Arturo Mendoza <arturo10.mr@gmail.com>
2017-10-15 10:54:44 -0500

    Kata Island V0

--- keyboard/key.c
+++ keyboard/key.c
@@ -2,21 +2,6 @@
 #include <stdio.h>
 #include "key.h"
 
-char isRow(char *words){
-  int *row1[9];
-  int *row2[8];
-  int *row3[6];
-  char row1a[9] = ["QERTYUIOP"];
-  char row2b = ["A","S","D","F","G","H","J","K","L"];
-  char row3c = ["Z","X","C","V","B","N","M"];
-
-  row1 = &row1a;
-
-
-
-  return "n";
-}
-
 int inRow(char *row, int length, char c) {
     for (int i = 0; i < length; i++) {
         int a = row[i];
