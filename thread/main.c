9dee7e46d7955d414d0fec5944fc253a343917f1
Luis Arturo Mendoza <arturo10.mr@gmail.com>
2017-11-09 19:13:49 -0600

    Added chairs phil problem

--- thread/main.c
+++ thread/main.c
@@ -8,6 +8,7 @@
 
 unsigned int total;
 sem_t forks[NUM_THREADS];
+sem_t chairs;
 
 void thinking(int n){
   sleep(rand()%n);
@@ -24,11 +25,15 @@ void *lifeStyle(void *threadid)
   while (1) {
     thinking(3);
     printf("I am ET %lu, I am hungry\n", tid);
+
     sem_wait(&forks[tid]);
     sem_wait(&forks[(tid +1) % NUM_THREADS]);
+    sem_wait(&chairs);
+
     eating(4);
     sem_post(&forks[tid]);
     sem_post(&forks[(tid +1) % NUM_THREADS]);
+    sem_post(&chairs);
     printf("I am ET %lu, I am thinking\n", tid);
 
   }
@@ -43,13 +48,14 @@ int main (int argc, char *argv[])
   for(t=0; t<NUM_THREADS; t++){
     sem_init(&forks[t],0,1);
   }
-    for(t=0; t<NUM_THREADS; t++){
-     printf("In main: creating thread %ld\n", t);
-     rc = pthread_create(&threads[t], NULL, lifeStyle, (void *)t);
-     if (rc){
-        printf("ERROR; return code from pthread_create() is %d\n", rc);
-        exit(-1);
-     }
+  sem_init(&chairs,0,NUM_THREADS/2);
+  for(t=0; t<NUM_THREADS; t++){
+   printf("In main: creating thread %ld\n", t);
+   rc = pthread_create(&threads[t], NULL, lifeStyle, (void *)t);
+   if (rc){
+      printf("ERROR; return code from pthread_create() is %d\n", rc);
+      exit(-1);
+   }
   }
   for (t = 0; t < NUM_THREADS; t++) {
     pthread_join(threads[t], NULL);
@@ -58,8 +64,3 @@ int main (int argc, char *argv[])
   /* Last thing that main() should do */
   pthread_exit(NULL);
 }
-
-5491380210151347
-10 20
-
-martha a reyes  mora
